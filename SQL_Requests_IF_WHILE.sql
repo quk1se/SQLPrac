1ae7f9ac8d5f0f0ce405f64d110c8b60692e0ab5
Alexandr Kalyan <110027812+quk1se@users.noreply.github.com>
2023-08-04 15:02:41 +0300

    Add files via upload

---
+++ SQL_Requests_IF_WHILE.sql
@@ -0,0 +1,113 @@
+﻿--1
+--DECLARE @time time
+--SET @time = CONVERT(TIME, GETDATE())
+--IF @time > '16:00:00:0' AND @time < '04:00:00:0' PRINT ('Äîáðûé âå÷åð')
+--ELSE PRINT('Äîáðûé äåíü')
+
+--2
+--DECLARE @password nvarchar(50)
+--DECLARE @count int
+--SET @count = 0
+--SET @password = ''
+--
+--WHILE @count < 10
+--BEGIN
+--	SET @password += CHAR(CAST(RAND() * 26 AS INT) + 97)
+--	SET @count += 1
+--END
+--PRINT(@password)
+
+--3
+--DECLARE @fact int
+--DECLARE @count int
+--SET @fact = 1
+--SET @count = 1
+--
+--WHILE @count <= 25
+--BEGIN
+--	SET @fact *= @count
+--	SET @count += 1
+--	PRINT (@fact)
+--END
+
+--4
+--DECLARE @count INT = 3;
+--DECLARE @isPrime int;
+--
+--WHILE @count <= 1000
+--BEGIN
+--    SET @isPrime = 1;
+--
+--    DECLARE @divs INT = 2;
+--    WHILE @divs <= SQRT(@count) AND @isPrime = 1
+--    BEGIN
+--        IF @count % @divs = 0
+--        BEGIN
+--            SET @isPrime = 0;
+--        END
+--
+--        SET @divs += 1;
+--    END
+--
+--    IF @IsPrime = 1
+--    BEGIN
+--        PRINT (@count)
+--    END
+--
+--    SET @count = @count + 1;
+--END
+
+
+--5
+DECLARE @money int
+DECLARE @bid int
+DECLARE @first_num int
+DECLARE @second_num int
+DECLARE @third_num int
+DECLARE @message nvarchar(50)
+
+SELECT @money = money
+FROM Casino_Data
+
+SET @bid = 10
+ 
+PRINT ('WELCOME TO CASINO ROULETTE!')
+IF @money > 0
+BEGIN
+	SET @message = 'Your balance: ' + CONVERT(NVARCHAR(50),(@money))
+	PRINT(@message)
+	PRINT('ROULETTE SPIN!')
+	SET @money -= @bid
+
+	SET @first_num = CAST(RAND() * 9 AS INT) + 1
+	SET @second_num = CAST(RAND() * 9 AS INT) + 1
+	SET @third_num = CAST(RAND() * 9 AS INT) + 1
+
+	IF @first_num = @second_num AND @second_num = @third_num AND @first_num = @second_num
+	BEGIN
+		SET @message = 'YOU WIN | YOUR NUMS: ' + CONVERT(NVARCHAR(50),(@first_num)) + CONVERT(NVARCHAR(50),(@second_num)) + CONVERT(NVARCHAR(50),(@third_num))
+		SET @money += 50
+		PRINT(@message)
+	END
+
+	ELSE
+	BEGIN
+		SET @message = 'YOU LOOSE | YOUR NUMS: ' + CONVERT(NVARCHAR(50),(@first_num)) + CONVERT(NVARCHAR(50),(@second_num)) + CONVERT(NVARCHAR(50),(@third_num))
+		PRINT(@message)
+	END
+	IF @first_num = 7 AND @second_num = 7 AND @third_num = 7
+	BEGIN
+		PRINT('YOU WIN!')
+		SET @money += 100000
+	END
+	UPDATE Casino_Data 
+	SET money = @money, first_num = @first_num, second_num = @second_num, third_num = @third_num
+	WHERE id_person = 1
+END
+ELSE PRINT ('NOT ENOUGH MONEY!')
+
+
+
+
+
+
